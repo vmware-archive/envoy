f6ce084dbf7c898261c7f5af9b561942f9b77258
Gabe Rosenhouse and Ryan Moran <rmoran@pivotal.io>
2015-03-02 10:40:01 -0800

    Splits Broker interface out into smaller interfaces

--- broker.go
+++ broker.go
@@ -3,10 +3,34 @@ package envoy
 import "github.com/pivotal-cf-experimental/envoy/domain"
 
 type Broker interface {
+	Cataloger
+	Credentialer
+	Provisioner
+	Binder
+	Unbinder
+	Deprovisioner
+}
+
+type Cataloger interface {
 	Catalog() domain.Catalog
+}
+
+type Credentialer interface {
 	Credentials() (string, string)
+}
+
+type Provisioner interface {
 	Provision(domain.ProvisionRequest) (domain.ProvisionResponse, error)
-	Bind(domain.BindRequest) (domain.BindResponse, error)
-	Unbind(domain.UnbindRequest) error
+}
+
+type Deprovisioner interface {
 	Deprovision(domain.DeprovisionRequest) error
+}
+
+type Binder interface {
+	Bind(domain.BindRequest) (domain.BindResponse, error)
+}
+
+type Unbinder interface {
+	Unbind(domain.UnbindRequest) error
 }
